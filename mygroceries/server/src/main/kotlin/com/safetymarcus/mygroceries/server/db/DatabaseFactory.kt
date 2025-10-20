6c5fb7db7d797530af19cfd258453db53b92045a
SafetyMarcus <hooperarma@gmail.com>
2025-10-20 23:33:21 +1000

    Working integration tests

--- mygroceries/server/src/main/kotlin/com/safetymarcus/mygroceries/server/db/DatabaseFactory.kt
+++ mygroceries/server/src/main/kotlin/com/safetymarcus/mygroceries/server/db/DatabaseFactory.kt
@@ -7,6 +7,7 @@ import org.jetbrains.exposed.sql.Database
 import org.flywaydb.core.Flyway
 
 import java.sql.DriverManager
+import org.postgresql.util.PSQLException
 
 object DatabaseFactory {
 
@@ -18,23 +19,35 @@ object DatabaseFactory {
     }
 
     private fun createDatabaseIfNotExists() {
+        val jdbcUrl = System.getenv("DB_JDBC_URL") ?: "jdbc:postgresql://localhost:5432/"
+        val username = System.getenv("DB_USERNAME") ?: "marcushooper"
+        val password = System.getenv("DB_PASSWORD") ?: ""
+
         try {
-            val connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/", "postgres", "postgres")
+            val connection = DriverManager.getConnection(jdbcUrl, username, password)
             val statement = connection.createStatement()
             statement.executeUpdate("CREATE DATABASE mygroceries")
             statement.close()
             connection.close()
+        } catch (e: PSQLException) {
+            if (e.getSQLState() == "42P04") {
+                println("Database 'mygroceries' already exists.")
+            } else {
+                System.err.println("Error creating database: ${e.message}")
+                throw e
+            }
         } catch (e: Exception) {
-            // Database already exists
+            System.err.println("Unexpected error creating database: ${e.message}")
+            throw e
         }
     }
 
     private fun hikari(): HikariDataSource {
         val config = HikariConfig()
         config.driverClassName = "org.postgresql.Driver"
-        config.jdbcUrl = "jdbc:postgresql://localhost:5432/mygroceries"
-        config.username = "postgres"
-        config.password = "postgres"
+        config.jdbcUrl = System.getenv("DB_JDBC_URL_WITH_DB") ?: "jdbc:postgresql://localhost:5432/mygroceries"
+        config.username = System.getenv("DB_USERNAME") ?: "postgres"
+        config.password = System.getenv("DB_PASSWORD") ?: "postgres"
         config.maximumPoolSize = 10
         config.isAutoCommit = false
         config.transactionIsolation = "TRANSACTION_REPEATABLE_READ"
