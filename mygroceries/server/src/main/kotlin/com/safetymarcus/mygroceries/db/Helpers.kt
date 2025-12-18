e655d1bb76395eec127f05f50bc91bc87d02a24b
SafetyMarcus <hooperarma@gmail.com>
2025-12-18 22:44:21 +1000

    Fixing transaction coroutine

--- mygroceries/server/src/main/kotlin/com/safetymarcus/mygroceries/db/Helpers.kt
+++ mygroceries/server/src/main/kotlin/com/safetymarcus/mygroceries/db/Helpers.kt
@@ -2,9 +2,8 @@ package com.safetymarcus.mygroceries.db
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import org.jetbrains.exposed.v1.jdbc.transactions.inTopLevelSuspendTransaction
-import org.jetbrains.exposed.v1.jdbc.transactions.transaction
+import org.jetbrains.exposed.v1.jdbc.transactions.suspendTransaction
 
 suspend fun <T> dbQuery(block: suspend () -> T): T = withContext(Dispatchers.IO) {
-    inTopLevelSuspendTransaction { block() }
+    suspendTransaction { block() }
 }
